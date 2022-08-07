a3c3724bbdc48861c936478b174dab7eefb74fb8
Sven Brodny <svenbrodny@googlemail.com>
2022-08-07 20:13:57 +0200

    Updated page title-strategy (article title)

--- src/app/core/shared/page.title-strategy.ts
+++ src/app/core/shared/page.title-strategy.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
+import { testArticles } from 'src/data/articles';
 
 @Injectable()
 export class PageTitleStrategy extends TitleStrategy {
   override updateTitle(snapshot: RouterStateSnapshot): void {
     const title = this.buildTitle(snapshot);
+    const isArticle = snapshot.url.includes('/articles/');
 
-    if (title) {
+    if (title && !isArticle) {
       document.title = 'ng-neer - ' + title;
+    } else if (isArticle) {
+      const article = testArticles.find((a) => snapshot.url.includes(a.url));
+      document.title = article?.title + ' - ng-neer' ?? 'ng-neer | Article';
     } else {
       document.title = 'ng-neer';
     }
